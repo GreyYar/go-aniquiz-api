34d2a8aeddb5e4321a9e8750215629a9fd863b03
Sergey Spasskiy <greyyar93@mail.ru>
2022-08-16 02:36:50 +0300

    FindByEmail tests

--- internal/app/store/user_repository_test.go
+++ internal/app/store/user_repository_test.go
@@ -17,3 +17,25 @@ func TestUserRepository_Create(t *testing.T) {
 	assert.NoError(t, err)
 	assert.NotNil(t, u)
 }
+
+func TestUserRepository_FindByEmail(t *testing.T) {
+	s, teardown := store.TestStore(t, databaseURL)
+	defer teardown("users")
+
+	email := "supa@example.org"
+
+	t.Run("No user found", func(t *testing.T) {
+		_, err := s.User().FindByEmail(email)
+		assert.Error(t, err)
+	})
+
+	t.Run("User found by email", func(t *testing.T) {
+		_, _ = s.User().Create(&model.User{
+			Email: email,
+		})
+
+		u, err := s.User().FindByEmail(email)
+		assert.NoError(t, err)
+		assert.NotNil(t, u)
+	})
+}
